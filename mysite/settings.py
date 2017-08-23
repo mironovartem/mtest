432611fc9f589c2dfd68dfb9a1570253fb6cc1c6
mironovartem <creativerror@gmail.com>
2017-08-23 17:05:55 +0400

    v1 20082017

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,12 +83,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+if database:
+    NAME = os.path.join(BASE_DIR, 'db.sqlite3')
+    ENGINE = 'django.db.backends.sqlite3'
+    HOST = ''
+    USER = ''
+    PASSWORD = ''
 
 DATABASES = {
             'default':{
-               'ENGINE' : 'django.db.backends.sqlite3',
-               'NAME' : os.path.join(BASE_DIR, 'db.sqlite3'),
+               'ENGINE' : ENGINE,
+               'NAME' : NAME,
+               'HOST': HOST,
+               'USER': USER,
+               'PASSWORD': PASSWORD
                },
 
 }
