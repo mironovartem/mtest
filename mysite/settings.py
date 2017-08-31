da7e1da2ec543058cc5ea83a0d2e8acf5b6688bb
mironovartem <creativerror@gmail.com>
2017-09-01 00:50:27 +0400

    2 01092017

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,5 +146,5 @@ if database:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
-    MEDIA_URL = 'http://media.testege.com/'
+    MEDIA_URL = 'http://www.testege.com/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
